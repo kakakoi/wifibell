32a309d3c501444a3d81bccd959100eeee4feec6
kakakoi <56661650kakakoi@users.noreply.github.com>
2021-02-25 09:34:12 +0900

    change ping image color

--- app/src/main/java/com/kakakoi/wifibell/ui/main/MainFragment.kt
+++ app/src/main/java/com/kakakoi/wifibell/ui/main/MainFragment.kt
@@ -1,17 +1,22 @@
 package com.kakakoi.wifibell.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.kakakoi.wifibell.R
 import com.kakakoi.wifibell.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+        const val TAG = "MainFragment"
     }
 
     private lateinit var viewModel: MainViewModel
@@ -22,16 +27,25 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
         binding = MainFragmentBinding.inflate(inflater, container, false)
         binding.vm = viewModel
         binding.lifecycleOwner = this
+
+        val signalLevelObserver = Observer<Int> { level ->
+            // Update the UI, in this case, a TextView.
+            binding.signalLevelText.setTextColor(Color.rgb(level, 0, 0))
+            if (level > 70) {
+                binding.pingImage.imageTintList =
+                    context?.getColorStateList(R.color.ping_color_state_list)
+                Log.d(TAG, "onCreateView: level > 70")
+            } else {
+                binding.pingImage.imageTintList =
+                    context?.getColorStateList(R.color.ping_color_warn_state_list)
+                Log.d(TAG, "onCreateView: level < 70")
+            }
+        }
+        viewModel.signalLevel.observe(viewLifecycleOwner, signalLevelObserver)
+
         return binding.root
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/kakakoi/wifibell/ui/main/MainViewModel.kt
+++ app/src/main/java/com/kakakoi/wifibell/ui/main/MainViewModel.kt
@@ -5,6 +5,8 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.net.*
 import android.net.wifi.WifiManager
 import android.os.Build
@@ -13,9 +15,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.kakakoi.wifibell.R
 
 
-class MainViewModel(application: Application) : AndroidViewModel(application),SensorEventListener {
+class MainViewModel(application: Application) : AndroidViewModel(application), SensorEventListener {
 
     companion object {
         const val SIGNAL_LEVEL = 100
@@ -63,15 +66,23 @@ class MainViewModel(application: Application) : AndroidViewModel(application),Se
     val rssiText: LiveData<String>
         get() = _rssiText
 
-    private var _signalLevel: MutableLiveData<String> =
+    private var _signalLevel: MutableLiveData<Int> =
+        MutableLiveData<Int>().also { mutableLiveData ->
+            mutableLiveData.value = 0
+        }
+    val signalLevel: LiveData<Int>
+        get() = _signalLevel
+
+    private var _signalLevelText: MutableLiveData<String> =
         MutableLiveData<String>().also { mutableLiveData ->
             mutableLiveData.value = "-"
         }
-    val signalLevel: LiveData<String>
-        get() = _signalLevel
+    val signalLevelText: LiveData<String>
+        get() = _signalLevelText
 
     init {
         load()
+        sound(application)
         val accel: Sensor = sm.getDefaultSensor(
             Sensor.TYPE_STEP_COUNTER
         )
@@ -79,20 +90,48 @@ class MainViewModel(application: Application) : AndroidViewModel(application),Se
         cm.registerNetworkCallback(request, networkCallback)
     }
 
-    private fun load(){
-        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
-
+    private fun load() {
         val info = wm.connectionInfo
 
         _rssi.value = info.rssi
         _rssiText.value = info.rssi.toString()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            _signalLevel.value = wm.calculateSignalLevel(info.rssi).toString()
+            _signalLevel.value = wm.calculateSignalLevel(info.rssi)
+            _signalLevelText.value = _signalLevel.value.toString()
         } else {
             _signalLevel.value =
-                WifiManager.calculateSignalLevel(info.rssi, SIGNAL_LEVEL).toString()
+                WifiManager.calculateSignalLevel(info.rssi, SIGNAL_LEVEL)
+            _signalLevelText.value = _signalLevel.value.toString()
+        }
+    }
+
+    private lateinit var soundPool: SoundPool
+    private var soundOne = 0
+
+    private fun sound(application: Application) {
+        val audioAttributes = AudioAttributes.Builder()
+            // USAGE_MEDIA
+            // USAGE_GAME
+            .setUsage(AudioAttributes.USAGE_GAME)
+            // CONTENT_TYPE_MUSIC
+            // CONTENT_TYPE_SPEECH, etc.
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            // ストリーム数に応じて
+            .setMaxStreams(2)
+            .build()
+
+        // one.wav をロードしておく
+        soundOne = soundPool.load(application, R.raw.pi, 1)
+
+        // load が終わったか確認する場合
+        soundPool.setOnLoadCompleteListener { soundPool, sampleId, status ->
+            Log.d("debug", "sampleId=$sampleId")
+            Log.d("debug", "status=$status")
         }
     }
 
@@ -106,11 +145,12 @@ class MainViewModel(application: Application) : AndroidViewModel(application),Se
 
     override fun onSensorChanged(event: SensorEvent?) {
         load()
+        // play(ロードしたID, 左音量, 右音量, 優先度, ループ, 再生速度)
+        soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
         Log.d(TAG, "onSensorChanged: ")
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        load()
         Log.d(TAG, "onAccuracyChanged: ")
     }
 }
